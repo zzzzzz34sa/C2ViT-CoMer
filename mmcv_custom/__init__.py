fbe587dc314223a59d11abb655e3b50741ec0b8f
zzzzzz34sa <152118899+zzzzzz34sa@users.noreply.github.com>
2025-05-09 16:21:09 +0800

    Add files via upload

---
+++ mmcv_custom/__init__.py
@@ -0,0 +1,10 @@
+from .checkpoint import load_checkpoint
+from .customized_text import CustomizedTextLoggerHook
+from .layer_decay_optimizer_constructor import LayerDecayOptimizerConstructor
+from .my_checkpoint import my_load_checkpoint
+
+__all__ = [
+    'LayerDecayOptimizerConstructor',
+    'CustomizedTextLoggerHook',
+    'load_checkpoint', 'my_checkpoint',
+]
